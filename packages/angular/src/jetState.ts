fd99ba4feaa0bd383a4c6cf8b62297e31e053095
Mikhail Nasyrov <streambot@gmail.com>
2019-02-14 13:56:40 +0700

    feat: Experimental support of patching a state by changes from Angular's component.

--- packages/angular/src/jetState.ts
+++ packages/angular/src/jetState.ts
@@ -1,7 +1,13 @@
+import {SimpleChanges} from '@angular/core';
 import {StateValueSelector} from '@jetstate/core';
 import {RxState} from './internal/rxState';
 import {JetProjection} from './jetProjection';
 import {JetProjectionBuilder} from './jetProjectionBuilder';
+
+/** @experimental */
+export type NgComponentInputMapper<Component, Model> = {
+    [inputKey in keyof Component]?: (value: Component[inputKey]) => Partial<Readonly<Model>> | undefined
+};
 
 export class JetState<Model extends object> extends RxState<Model> {
     static readonly compute = JetProjectionBuilder.from;
@@ -20,5 +26,25 @@ export class JetState<Model extends object> extends RxState<Model> {
 
     pick<V>(selector: StateValueSelector<Model, V>): JetProjection<V> {
         return super.pick(selector);
+    }
+
+    /** @experimental */
+    patchByNgChanges<Component>(changes: SimpleChanges, inputs: NgComponentInputMapper<Component, Model>) {
+        let newState: Model | undefined;
+        const inputKeys = Object.getOwnPropertyNames(inputs);
+        for (const inputKey of inputKeys) {
+            const change = changes[inputKey];
+            const patchCallback = (inputs as any)[inputKey];
+            if (!change || !patchCallback) {
+                continue;
+            }
+            const stateUpdate = patchCallback(change.currentValue);
+            if (stateUpdate) {
+                newState = Object.assign(newState || {}, stateUpdate);
+            }
+        }
+        if (newState) {
+            this.patch(newState);
+        }
     }
 }
