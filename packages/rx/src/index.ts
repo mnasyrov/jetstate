c150c55d08e00761d06e4ea19839ee28ac735b53
Mikhail Nasyrov <streambot@gmail.com>
2019-08-19 08:26:03 +0700

    refactor:

--- packages/rx/src/index.ts
+++ packages/rx/src/index.ts
@@ -1,2 +1,1 @@
-export * from './rxProjection';
 export * from './rxState';
--- packages/rx/src/rxState.ts
+++ packages/rx/src/rxState.ts
@@ -1,6 +1,46 @@
-import {Selector, State} from '@jetstate/core';
+import {Consumer, Projection, Selector, State} from '@jetstate/core';
 import {Observable} from 'rxjs';
-import {createRxProjection, RxProjection} from './rxProjection';
+
+export interface RxProjection<V> extends Projection<V> {
+  readonly current$: Observable<V>;
+  readonly changes$: Observable<V>;
+}
+
+export function createRxProjection<V>(
+  projection: Projection<V>,
+): RxProjection<V> {
+  let current$: Observable<V>;
+  let changes$: Observable<V>;
+
+  return {
+    get current(): V {
+      return projection.current;
+    },
+
+    get current$(): Observable<V> {
+      if (!current$) {
+        current$ = new Observable<V>(subscriber => {
+          subscriber.next(projection.current);
+          return projection.listenChanges(value => subscriber.next(value));
+        });
+      }
+      return current$;
+    },
+
+    get changes$(): Observable<V> {
+      if (!changes$) {
+        changes$ = new Observable<V>(subscriber => {
+          return projection.listenChanges(value => subscriber.next(value));
+        });
+      }
+      return changes$;
+    },
+
+    listenChanges(consumer: Consumer<V>) {
+      return projection.listenChanges(consumer);
+    },
+  };
+}
 
 export class RxState<Model extends object> extends State<Model>
   implements RxProjection<Readonly<Model>> {
