f65883d95781a0c50ad2628b3c9e8fd4365911e8
Mikhail Nasyrov <streambot@gmail.com>
2019-10-28 00:59:47 +0700

    chore: Fixed bundle building.

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
+
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.ts',
+    external: ['rxjs', 'rxjs/operators'],
+    plugins: [typescript()],
+    output: [{file: pkg.main, format: 'cjs'}, {file: pkg.module, format: 'es'}],
+  },
+];
